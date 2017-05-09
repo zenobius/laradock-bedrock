128c9ab024c6db777217c284f78c1d721ff5d660
Lukas Besch <connect@lukasbesch.com>
2017-05-09 16:16:38 +0200

    bedrock adjustments

---
+++ mariadb/docker-entrypoint-initdb.d/createdb.sql
@@ -0,0 +1,10 @@
+###
+### Copy createdb.sql.example to createdb.sql
+### then uncomment and set database name to create you need databases
+###
+
+#CREATE DATABASE IF NOT EXISTS `dev_db_1` ;
+#CREATE DATABASE IF NOT EXISTS `dev_db_2` ;
+#CREATE DATABASE IF NOT EXISTS `dev_db_3` ;
+
+CREATE DATABASE IF NOT EXISTS `default` ;
